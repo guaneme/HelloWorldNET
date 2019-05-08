89cd49f4e55226c9efaed679f21f0952c77b8bff
Carlos Guaneme <carlosguaneme@eiu.com>
2019-05-08 16:47:10 -0400

    Adding tests project (.NET framework)

---
+++ HelloWorldConsoleNET/HelloWorldConsoleTestsNET/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloWorldConsoleTestsNET")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HelloWorldConsoleTestsNET")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9e37a263-768f-45c4-a4ec-30c5df214ab6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HelloWorldConsoleNET/HelloWorldConsoleTestsNET/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorldConsoleTestsNET
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
